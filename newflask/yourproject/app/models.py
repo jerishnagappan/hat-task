789b02c718f12e97ed8b18525baf29e0ac19e68e
jerishnagappan <jerish.nagappan@hashagile.com>
2024-06-12 13:47:11 +0530

    Delete newflask/yourproject/app/models.py

--- newflask/yourproject/app/models.py
+++
@@ -1,8 +0,0 @@
-from yourproject.app.extensions import db
-
-class Project(db.Model):
-    __tablename__ = 'projects'
-    
-    __table_args__ = {'schema': 'jerish'}
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True, nullable=False)
