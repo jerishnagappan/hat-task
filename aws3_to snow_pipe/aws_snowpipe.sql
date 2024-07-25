1a4a394b0e5b514b9481706d8b617dfcbd112805
jerishnagappan <jerish.nagappan@hashagile.com>
2024-07-25 18:50:44 +0530

    Add files via upload

---
+++ aws3_to snow_pipe/aws_snowpipe.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE STORAGE INTEGRATION s3_int
+    TYPE = EXTERNAL_STAGE
+    STORAGE_PROVIDER = 's3'
+    ENABLED = TRUE
+    STORAGE_AWS_ROLE_ARN = 'arn:....................'
+    STORAGE_ALLOWED_LOCATIONS = ('s3://snowflake/');
+ 
+ 
+desc integration s3_int;
+ 
+create or replace database snowpipe;
+ 
+create or replace stage snowstage
+url = 's3://snowflake/'
+storage_integration = s3_int;
+ 
+show stages;
+ 
+CREATE OR REPLACE TABLE snowpipe.public.snowtable(jsontext variant);
+ 
+select * from snowtable;
+ 
+CREATE OR REPLACE PIPE snowpipe.public.snowpipe auto_ingest=true as
+copy into snowpipe.public.snowtable
+from @snowpipe.public.snowstage
+file_format=(type = 'JSON');
+show pipes;
+ 
+select SYSTEM$PIPE_STATUS('snowpipe.public.snowpipe');   
+ 
+select * from snowpipe.public.snowtable;
+ 
+select * from table(information_schema.copy_history(table_name=>'snowpipe.public.snowtable',start_time=>dateadd(hours,-1,current_timestamp())));
+ 
